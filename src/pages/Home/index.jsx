b47cc5d9aac73eb320d752bb861482de45a8a984
cadugithub <carlosk2k2@gmail.com>
2022-08-30 11:51:03 -0300

    ponto

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,7 +5,7 @@ export function Home(){
     return(
         <div className="home">
             <h1>Jokenpô<img src='/img/Pedra-Papel-Tesoura.svg'></img></h1> 
-            <p>Pedra, papel e tesoura, também chamado em algumas regiões do Brasil de jokenpô, é um jogo de mãos recreativo e simples para duas ou mais pessoas, que não requer equipamentos nem habilidade</p>
+            <p>Pedra, papel e tesoura, também chamado em algumas regiões do Brasil de jokenpô, é um jogo de mãos recreativo e simples para duas ou mais pessoas, que não requer equipamentos nem habilidade.</p>
             <p><strong>Regras:</strong> <br/> Papel ganha de pedra; <br/> Pedra ganha de Tesoura; <br/>Tesoura ganha de papel</p>
             <Link to="/escolhaPersonagem"><Button >JOGAR</Button></Link>
         </div>
